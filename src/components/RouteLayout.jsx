9d3838d42bad7de1d6399cbb052b5b1d6851057d
sireesha <balusireesha29@gmail.com>
2023-07-16 10:53:00 +0530

    commir_16/07/23_10:53AM

---
+++ src/components/RouteLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Outlet} from 'react-router-dom';
+import Navi from './NavigationBar/Navi';
+import Footer from './Footer/Footer';
+
+function RouteLayout() {
+
+  return (
+    <div>
+      <Navi/>
+    
+      <Outlet/>
+    
+      <Footer/>
+    </div>
+  )
+}
+
+export default RouteLayout;
