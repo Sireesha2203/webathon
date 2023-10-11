0a7d859b9cd6e41a50f8fbf467f1a743e7960389
sireesha <balusireesha29@gmail.com>
2023-10-11 21:30:50 +0530

    11/10/23 21:30

--- src/components/Entrepreneurship/Entrepreneurship.jsx
+++ src/components/Entrepreneurship/Entrepreneurship.jsx
@@ -33,6 +33,11 @@ function Entrepreneurship() {
             <div  className="container container1 fluid ">
               <div className="row row-cols-1 row-cols-lg-1 ">
                   <div className="col c1">
+                      <label htmlFor="username">Username</label>
+                      <input type="text" name="username" id="username" placeholder='Enter Username' className="form-control mx-auto" {...register("ideaname",{required:{value:"true",message:"* Ideaname is required"},minLength:{value:4,message:"* Ideaname is Too Small"},maxLength:{value:600,message:"* Ideaname is Too Big"}})} />
+                      {errors.ideaname?.message &&<p className="text-danger">{errors.ideaname?.message}</p>}
+                  </div>
+                  <div className="col c1">
                       <label htmlFor="ideaname">Idea Name</label>
                       <input type="text" name="ideaname" id="ideaname" placeholder='Enter your Idea Name' className="form-control mx-auto" {...register("ideaname",{required:{value:"true",message:"* Ideaname is required"},minLength:{value:4,message:"* Ideaname is Too Small"},maxLength:{value:600,message:"* Ideaname is Too Big"}})} />
                       {errors.ideaname?.message &&<p className="text-danger">{errors.ideaname?.message}</p>}
@@ -42,48 +47,21 @@ function Entrepreneurship() {
                     <textarea name="idea" id="idea" cols="10" rows="10" placeholder='Describe your Idea here' className="form-control mx-auto" {...register("idea",{required:{value:"true",message:"* Idea is required"},minLength:{value:4,message:"* Idea is Too Small"},maxLength:{value:6000,message:"* Idea is Too Big"}})}></textarea>
                     {errors.idea?.message &&<p className="text-danger">{errors.idea?.message}</p>}
                   </div>
-                  {/* <div className="col c1">
-                      <label htmlFor="mail">Email</label>
-                      <input type="email" name="mail" id="mail" className="form-control" {...register("mail",{required:{value:"true",message:"* Email is required"}})}/>
-                      {errors.mail?.message &&<p className="text-danger">{errors.mail?.message}</p>}
-                  </div> */}
-                 
-                 
-             
-              
-              {/* <div className="row row-cols-1">
-                  <div className="col c1">
-                      <label htmlFor="subject">Subject</label>
-                      <select className="form-select" name="subject" id="subject" {...register("subject",{required:{value:"true",message:"* Please select Subject"}})} defaultValue="" >
-                          <option value="" disabled hidden>Choose Subject...</option>
-                          <option value="cse">CSE</option>
-                          <option value="ece">ECE</option>
-                          <option value="eee">EEE</option>
-                          <option value="other">Other...</option>
-                      </select>
-                      {errors.subject?.message &&<p className="text-danger">{errors.subject?.message}</p>}
+        
+                  <div className='col c1' >
+                    <label htmlFor="link"   style={{cursor:'pointer'}}>
+                      Paste Your Video Link here...
+                    </label>
+                    <input type="text" id="link"name="link" className="form-control" {...register("link",{required: {value:"true",message:"* Video Link is required"}})}/>
+                    {errors.link?.message &&<p className="text-danger">{errors.link?.message}</p>}
+                    <br />
                   </div>
-              </div> */}
-              <div className="col c1">
-              <input type="text" className='form-control rounded mb-2' placeholder='Username' {...register("username",{required:{value:true,message:"* Username is required"},minLength:{value:4,message:"* please type atleast 4 characters"},maxLength:{value:20,message:"* Sorry you cannot use more than 20 characters"}})}>
-              </input>
-              {errors.username?.message ? <p className="text-danger">{errors.username?.message}</p>:<p className='mt-2'></p>}
-      
-              </div>
-              <div className='col c1' >
-                <label htmlFor="link"   style={{cursor:'pointer'}}>
-                  Paste Your Video Link here...
-                </label>
-                <input type="text" id="link"name="link" className="form-control" {...register("link",{required: {value:"true",message:"* Video Link is required"}})}/>
-                {errors.link?.message &&<p className="text-danger">{errors.link?.message}</p>}
-                <br />
-              </div>
 
-              <div className=" row row-cols-6 mb-5">
-                  <div className=" col c1 mx-auto">
-                      <button  className="btn btn1 " type="submit">Submit</button>
+                  <div className=" row row-cols-6 mb-5">
+                      <div className=" col c1 mx-auto">
+                          <button  className="btn btn1 " type="submit">Submit</button>
+                      </div>
                   </div>
-              </div>
               </div>
             </div>
           </form>       
